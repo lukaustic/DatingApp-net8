80624bd8db7ab3f2a91893e65acf47abc5863b8e
lukaustic <lukaustic1415@gmail.com>
2024-12-07 17:35:17 +0100

    Adding the likes feature

---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class UserLike
+    {
+        public User SourceUser { get; set; } = null!;
+        public int SourceUserId { get; set; }
+        public User TargetUser { get; set; } = null!;
+        public int TargetUserId { get; set; }
+    }
+}
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.DTOs;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
@@ -18,6 +19,7 @@ namespace API.Extensions
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
